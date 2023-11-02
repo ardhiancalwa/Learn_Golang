bb8e62566d824836d6140ac7e83f07d452826143
ardhiancalwa <ardhiancalwa886@gmail.com>
2023-11-02 08:12:42 +0700

    update

--- kuis/perulangan/konsekutif.go
+++ kuis/perulangan/konsekutif.go
@@ -8,7 +8,7 @@ func main()  {
 	fmt.Scan(&X)
 	konsekutif = true
 	for X >= 10 {
-		konsekutif = ( X % 10 - X / 10 == 1) || (X / 10 - X % 10 == 1)
+		konsekutif = konsekutif && ((X % 10) - (X / 10 % 10) == 1) || ((X / 10 % 10) - (X % 10) == 1) 
 		X /= 10
 	}
 	fmt.Println(konsekutif)
--- kuis/perulangan/rata_ngoding.go
+++ kuis/perulangan/rata_ngoding.go
@@ -5,16 +5,14 @@ func main()  {
 	var n, totalJam, jam int
 	var rata_rata float64
 	
+	fmt.Scan(&jam)
 	n = 0
 	totalJam = 0
-	for {
-		fmt.Scan(&jam)
-		if jam < 0 {
-			break
-		}
+	for jam >= 0{
 		totalJam += jam
 		n++
 		rata_rata = float64(totalJam) / float64(n)
+		fmt.Scan(&jam)
 	}
 	fmt.Printf("%.2f", rata_rata)
 }
