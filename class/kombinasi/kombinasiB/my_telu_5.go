8876c1a25543207ee6eb4d664091c3148780c773
ardhiancalwa <ardhiancalwa886@gmail.com>
2023-12-22 11:13:33 +0700

    update

--- class/kombinasi/kombinasiB/my_telu_5.go
+++ class/kombinasi/kombinasiB/my_telu_5.go
@@ -1,22 +1,66 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var poin, jumPoin int
+// 	var gold, platinum, silver int
+// 	var poinGold, poinPlatinum, poinSilver int
+// 	var rataGold, rataPlatinum, rataSilver float64
+
+// 	fmt.Scan(&poin)
+// 	for poin < 50 {
+// 		fmt.Scan(&poin)
+// 	}
+// 	if poin > 200 {
+// 		poinGold += poin
+// 		gold++
+// 	} else if poin >= 100 {
+// 		poinPlatinum += poin
+// 		platinum++
+// 	} else if poin >= 50 {
+// 		poinSilver += poin
+// 		silver++
+// 	}
+// 	jumPoin += poin
+// 	if jumPoin > 500 {
+// 		break
+// 	}
+// 	if gold == 0 {
+// 		gold = 1
+// 	} else if platinum == 0 {
+// 		platinum = 1
+// 	} else if silver == 0 {
+// 		silver = 1
+// 	}
+// 	rataGold = float64(poinGold) / float64(gold)
+// 	rataPlatinum = float64(poinPlatinum) / float64(platinum)
+// 	rataSilver = float64(poinSilver) / float64(silver)
+// 	fmt.Println("gold: ", rataGold)
+// 	fmt.Println("platinum: ", rataPlatinum)
+// 	fmt.Println("silver: ", rataSilver)
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	var poin, jumPoin, i, n int
-	var gold, platinum, silver int
+	var poin, jumPoin, gold, platinum, silver int
 	var poinGold, poinPlatinum, poinSilver int
-	var newUser bool
 	var rataGold, rataPlatinum, rataSilver float64
+	fmt.Scan(&poin)
 
-	fmt.Scan(&n)
-	for i = 1; i <= n; i++ {
-		fmt.Scan(&poin, &newUser)
-		for poin < 50 {
-			fmt.Scan(&poin, &newUser)
-		}
-		if newUser {
-			poin += 50
+	jumPoin = 0
+	gold = 0
+	platinum = 0
+	silver = 0
+	if poin > 500 {
+		gold = 1
+	}
+	if jumPoin <= 500 {
+		jumPoin += poin
+		for jumPoin <= 500 {
 			if poin > 200 {
 				poinGold += poin
 				gold++
@@ -27,34 +71,28 @@ func main() {
 				poinSilver += poin
 				silver++
 			}
-		} else {
-			if poin > 200 {
-				poinGold += poin
-                gold++
-			} else if poin >= 100 {
-				poinPlatinum += poin
-                platinum++
-			} else if poin >= 50 {
-				poinSilver += poin
-                silver++
-			}
+			fmt.Scan(&poin)
+			jumPoin += poin
 		}
-		jumPoin += poin
-		if jumPoin > 500 {
-			break
-		}
-		if gold == 0 {
-			gold = 1
-		} else if platinum == 0 {
-			platinum = 1
-        } else if silver == 0 {
-			silver = 1
-		}
-		rataGold = float64(poinGold)/float64(gold)
-		rataPlatinum = float64(poinPlatinum)/float64(platinum)
-		rataSilver = float64(poinSilver)/float64(silver)
 	}
-	fmt.Println("gold: ", rataGold)
-	fmt.Println("platinum: ", rataPlatinum)
-	fmt.Println("silver: ", rataSilver)
+	if gold == 0 {
+		gold = 1
+	}
+	if platinum == 0 {
+		platinum = 1
+	}
+	if silver == 0 {
+		silver = 1
+	}
+
+	rataGold = float64(poinGold) / float64(gold)
+	rataPlatinum = float64(poinPlatinum) / float64(platinum)
+	rataSilver = float64(poinSilver) / float64(silver)
+
+	fmt.Println("Gold: ", rataGold)
+	fmt.Println("Platinum: ", rataPlatinum)
+	fmt.Println("Silver: ", rataSilver)
+
+	fmt.Println(poinGold, gold)
+	
 }
