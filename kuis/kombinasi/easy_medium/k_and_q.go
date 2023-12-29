837c25fc16c09d26ad5901d8f64b3b2e43fad227
ardhiancalwa <ardhiancalwa886@gmail.com>
2023-12-29 10:09:44 +0700

    update

--- kuis/kombinasi/easy_medium/k_and_q.go
+++ kuis/kombinasi/easy_medium/k_and_q.go
@@ -14,6 +14,6 @@ func main() {
 			break
 		}
 	}
-
+	
 	fmt.Println(hasil)
 }
--- kuis/kombinasi/easy_medium/suit.go
+++ kuis/kombinasi/easy_medium/suit.go
@@ -1,1 +1,21 @@
-c
+package main
+
+import "fmt"
+
+func main() {
+	var i, n int
+	var A, B int
+	var pemain1, pemain2 byte
+
+	fmt.Scan(&n)
+	
+	for i = 1; i <= n; i++ {
+		fmt.Scanf("%c%c", &pemain1, &pemain2)
+		if (pemain1 == 'g' && pemain2 == 'k') || (pemain1 == 'b' && pemain2 == 'g') || (pemain1 == 'k' && pemain2 == 'b') {
+			A++
+		} else if (pemain1 == 'k' && pemain2 == 'g') || (pemain1 == 'g' && pemain2 == 'b') || (pemain1 == 'b' && pemain2 == 'k'){
+			B++
+		}
+	}
+	fmt.Println(A, B)
+}
