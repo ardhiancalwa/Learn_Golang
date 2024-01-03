88df1322fc81b4e696a0e8739e53d8ae6237636b
ardhiancalwa <ardhiancalwa886@gmail.com>
2024-01-03 07:35:53 +0700

    update

--- kuis/kombinasi/hard/prima.go
+++ kuis/kombinasi/hard/prima.go
@@ -3,18 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var prima bool = true
-	var bil, i int
+	var prima bool
+	var bil, i, jumBil int
 
 	fmt.Scan(&bil)
-	if bil <= 1 {
-		prima = false
-	} else {
-		for i = 2; i*i <= bil; i++ {
-			if bil%i == 0 {
-                prima = false
-            }
-        }
+	for i = 2; i <= bil; i++ {
+		if bil%i == 0 {
+			jumBil++
+		}
 	}
+	prima = jumBil == 1 || jumBil == bil
 	fmt.Println(prima)
 }
